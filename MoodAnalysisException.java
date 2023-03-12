8d35a46f27d41b20f52cd709df92316a44eb2031
SUDHEER341 <sudheer601220@gmail.com>
2023-03-12 07:59:50 +0530

    Added : Change the mood dynamically

--- MoodAnalysisException.java
+++ MoodAnalysisException.java
@@ -1,9 +1,23 @@
 package com.moodanalyser;
 
-public class MoodAnalysisException extends Throwable {
-    public static Object exceptionType;
-
-    public MoodAnalysisException(Object p0, String pleaseEnterMood) {
-
+public class MoodAnalysisException extends Exception {
+    public exceptionType myException;
+    public MoodAnalysisException(exceptionType type, String messgae, Throwable cause) {
+        super(messgae);
+        new MoodAnalysisException(type, cause);
+    }
+    public MoodAnalysisException(exceptionType type, Throwable cause) {
+        super(cause);
+        this.myException = type;
+    }
+    public MoodAnalysisException(exceptionType type, String message)
+    {
+        super(message);
+        this.myException = type;
+    }
+    public enum exceptionType
+    {
+        ENTERED_NULL, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE, ENTERED_EMPTY
     }
 }
+
--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,30 +1,46 @@
 package com.moodanalyser;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzer {
-    public String message;
+    private String message;
 
-    public MoodAnalyzer(String message) {
+    public MoodAnalyzer(String message)
+    {
         this.message = message;
     }
 
-    public String analysingMood(String message) throws MoodAnalysisException {
+    public String analyseMood(String message) throws MoodAnalysisException
+    {
         this.message = message;
-        return analysingMood();
+        return analyseMood();
     }
 
-    public String analysingMood() throws MoodAnalysisException {
-        try {
+    public String analyseMood() throws MoodAnalysisException
+    {
+        try
+        {
             if (message.length() == 0)
-                throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_EMPTY, "Please enter Proper Mood");
+                throw new MoodAnalysisException(MoodAnalysisException
+                            .exceptionType.ENTERED_EMPTY, "Please enter Proper Mood");
             if (message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
         }
+
         catch (NullPointerException e)
         {
-            throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_NULL, "Please enter Proper Mood");
+            throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_NULL, e);
         }
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        MoodAnalyzer mood = (MoodAnalyzer) other;
+        return this.message.equals(mood);
     }
 }
 
